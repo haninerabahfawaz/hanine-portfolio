ae776715b68a761747dba0307ccf28d34d0906d2
haninerabahfawaz <haninefawaz430@gmail.com>
2025-05-22 14:43:21 +0300

    add theme toggle swithc

--- src/components/NavBarSection.js
+++ src/components/NavBarSection.js
@@ -20,64 +20,67 @@ const NavBarSection = () => {
 
   return (
     <Navbar
-      style={{ backgroundColor: darkMode ? '#333' : '#7d6648' }}
-      variant="dark"
-      expand="lg"
-      sticky="top"
-    >
-      <Container>
-        <Navbar.Brand
-          as={NavLink}
-          to="/"
-          className="nav-portfolio d-flex align-items-center"
-          style={{ marginLeft: '0px' }}
-        >
-          <img
-            src={HanineLogo}
-            alt="Logo"
-            width="50"
-            height="50"
-            className="d-inline-block align-top me-2"
-          />
-          Hanine Fawaz
-        </Navbar.Brand>
-
+    style={{ backgroundColor: darkMode ? '#333' : '#7d6648' }}
+    variant="dark"
+    expand="lg"
+    sticky="top"
+  >
+    <Container className="d-flex align-items-center justify-content-between">
+      {/* Brand */}
+      <Navbar.Brand
+        as={NavLink}
+        to="/"
+        className="nav-portfolio d-flex align-items-center"
+      >
+        <img
+          src={HanineLogo}
+          alt="Logo"
+          className="nav-img-logo d-inline-block align-top me-1"
+        />
+        Hanine Fawaz
+      </Navbar.Brand>
+  
+      {/* Collapse Toggle + Theme Toggle on small screens */}
+      <div className="d-flex align-items-center d-lg-none">
+        {/* Theme Toggle - Small Screens Only */}
+        <div onClick={toggleTheme} className="me-2">
+          <div className={`dual-icon-toggle ${darkMode ? 'dark' : 'light'}`}>
+            <FaSun size={14} className="sun-icon" />
+            <FaMoon size={14} className="moon-icon" />
+            <div className="toggle-thumb"></div>
+          </div>
+        </div>
+  
+        {/* Navbar Toggle */}
         <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto nav-font align-items-center">
-            <Nav.Link as={NavLink} to="/" end className='nav-link'>Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/skills" className='nav-link'>Skills</Nav.Link>
-            <Nav.Link as={NavLink} to="/projects" className='nav-link'>Projects</Nav.Link>
-            <Nav.Link as={NavLink} to="/contact" className='nav-link'>Contact</Nav.Link>
-            
-            {/* Theme Toggle Icon */}
-            <Nav.Link
-              onClick={toggleTheme}
-              className="theme-toggle-link ms-lg-5 d-flex align-items-center"
-            >
-              {darkMode ? (
-                <>
-                  <div className='toggle-text'>
-                      <span className="d-inline d-sm-none me-2"
-                        style={{}}>
-                        Light Mode 
-                      </span>
-                      <FaSun size={16} className="me-1" />
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div  className='toggle-text'>
-                    <span className="d-inline d-sm-none me-2"> Dark Mode </span>
-                    <FaMoon size={16} className="me-1" />
-                  </div>
-                </>
-              )}
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      </div>
+  
+      {/* Collapsible Menu */}
+      <Navbar.Collapse id="navbar-nav">
+        <Nav className="ms-auto nav-font align-items-center">
+          <Nav.Link as={NavLink} to="/" end className="nav-link">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/skills" className="nav-link">Skills</Nav.Link>
+          <Nav.Link as={NavLink} to="/projects" className="nav-link">Projects</Nav.Link>
+          <Nav.Link as={NavLink} to="/contact" className="nav-link">Contact</Nav.Link>
+  
+          {/* Theme Toggle - Large Screens Only */}
+          <Nav.Link
+            onClick={toggleTheme}
+            className="theme-toggle-link ms-lg-5 d-none d-lg-flex align-items-center"
+          >
+            <div className="toggle-wrapper d-flex align-items-center">
+              <div className={`dual-icon-toggle ${darkMode ? 'dark' : 'light'}`}>
+                <FaSun size={14} className="sun-icon" />
+                <FaMoon size={14} className="moon-icon" />
+                <div className="toggle-thumb"></div>
+              </div>
+            </div>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  
   );
 };
 
